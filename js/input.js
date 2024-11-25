efb6044171ccb16eb0e802811574e202d7f01c0c
Marko Vojinovic <146836999+Vojinovic-M@users.noreply.github.com>
2024-11-26 00:37:03 +0100

    input entity implemented

--- js/input.js
+++ js/input.js
@@ -1,3 +1,4 @@
+// entitet za mapiranje pravaca
 const Direction = {
     UP: { x: 0, y: -1, opposite: 'DOWN' },
     DOWN: { x: 0, y: 1, opposite: 'UP' },
@@ -16,29 +17,9 @@ export const handleInput = (state, key) => {
     // kopija trenutnog stanja za kasnije menjanje
     const newState = { ...state };
 
-    // pravac se menja samo ako zmija ne ide unazad
-    switch (key) {
-        case 'ArrowUp':
-            if (newState.direction.y !== 1) { // ne moze da ide dole
-                newState.direction = { x: 0, y: -1 };
-            }
-            break;
-        case 'ArrowDown':
-            if (newState.direction.y !== -1) { // ne moze da ide gore
-                newState.direction = { x: 0, y: 1 };
-            }
-            break;
-        case 'ArrowLeft':
-            if (newState.direction.x !== 1) { // ne moze da ide desno
-                newState.direction = { x: -1, y: 0 };
-            }
-            break;
-        case 'ArrowRight':
-            if (newState.direction.x !== -1) { // ne moze da ide levo
-                newState.direction = { x: 1, y: 0 };
-            }
-            break;
+    const newDirection = directionMap[key];
+    if (newDirection && newDirection.opposite !== state.direction.opposite) {
+        newState.direction = newDirection;
     }
-
     return newState;
 };
