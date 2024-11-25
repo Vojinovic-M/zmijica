4d9ac55711a849c4410dc56bece4eb0b84817ac1
Marko Vojinovic <146836999+Vojinovic-M@users.noreply.github.com>
2024-11-26 00:32:53 +0100

    added new wallpaper, adding input entity

--- js/input.js
+++ js/input.js
@@ -1,3 +1,17 @@
+const Direction = {
+    UP: { x: 0, y: -1, opposite: 'DOWN' },
+    DOWN: { x: 0, y: 1, opposite: 'UP' },
+    LEFT: { x: -1, y: 0, opposite: 'RIGHT' },
+    RIGHT: { x: 1, y: 0, opposite: 'LEFT' }
+};
+
+const directionMap = {
+    'ArrowUp': Direction.UP,
+    'ArrowDown': Direction.DOWN,
+    'ArrowLeft': Direction.LEFT,
+    'ArrowRight': Direction.RIGHT
+};
+
 export const handleInput = (state, key) => {
     // kopija trenutnog stanja za kasnije menjanje
     const newState = { ...state };
